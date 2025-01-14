20269e0bedd10fe57ff2b57fb4b701263a2f8ca0
Sirpedia <carsonbutler@uagateway.org>
2025-01-14 09:55:43 -0500

    Create node.js

---
+++ node.js
@@ -0,0 +1,6 @@
+// Save data to local storage
+localStorage.setItem('key', 'value');
+
+// Retrieve data from local storage
+let data = localStorage.getItem('key');
+console.log(data); // Output: value
